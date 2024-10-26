05c2b6f4069ccc4b686b6344eaf457c0e7533d37
Norman Qian <normanqian@gmail.com>
2024-10-26 01:17:31 -0400

    rag

--- index.ts
+++ index.ts
@@ -1,10 +1,69 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import createRagger, {
+  NomicEmbedder,
+  PostgresVectorStore,
+  MinioDocStore,
+  Document,
+} from "@forge-ml/rag";
 
 dotenv.config();
 
 const app = express();
+
+const nomicApiKey = process.env.NOMIC_API_KEY;
+if (!nomicApiKey) {
+  throw new Error("NOMIC_API_KEY is not set in the environment variables");
+}
+
+const databaseURL = process.env.DATABASE_URL;
+if (!databaseURL) {
+  throw new Error("DATABASE_URL is not set in the environment variables");
+}
+
+const minioEndpoint = process.env.AH_S3_OBJECT_STORAGE_STACKHERO_SILVER_HOST;
+if (!minioEndpoint) {
+  throw new Error("AH_S3_OBJECT_STORAGE_STACKHERO_SILVER_HOST is not set");
+}
+
+const minioAccessKey =
+  process.env.AH_S3_OBJECT_STORAGE_STACKHERO_SILVER_ROOT_ACCESS_KEY;
+if (!minioAccessKey) {
+  throw new Error(
+    "AH_S3_OBJECT_STORAGE_STACKHERO_SILVER_ROOT_ACCESS_KEY is not set"
+  );
+}
+
+const minioSecretKey =
+  process.env.AH_S3_OBJECT_STORAGE_STACKHERO_SILVER_ROOT_SECRET_KEY;
+if (!minioSecretKey) {
+  throw new Error(
+    "AH_S3_OBJECT_STORAGE_STACKHERO_SILVER_ROOT_SECRET_KEY is not set"
+  );
+}
+
+const embedder = new NomicEmbedder({
+  type: "nomic",
+  apiKey: nomicApiKey,
+});
+
+const postgresVectorStore = new PostgresVectorStore(databaseURL);
+
+const minioDocStore = new MinioDocStore({
+  endpoint: minioEndpoint,
+  accessKey: minioAccessKey,
+  secretKey: minioSecretKey,
+});
+
+//in order to create a ragger we need:
+// 1. embedder
+// 2. vectorStore
+// 3. docStore
+const ragger = createRagger(embedder, {
+  vectorStore: postgresVectorStore,
+  docStore: minioDocStore,
+});
 
 // More permissive CORS configuration
 app.use(
@@ -23,6 +82,14 @@ const API_URL = "https://api.lambdalabs.com/v1/chat/completions";
 // Add a simple GET route for testing
 app.get("/", (req, res) => {
   res.send("Backend is running");
+});
+
+// Initialize a document and return the chunks
+app.get("/rag", async (req, res) => {
+  const chunks = await ragger.initializeDocument(
+    new Document("Norman has 10 apples, 2000 oranges, and 77 bananas.")
+  );
+  res.json(chunks);
 });
 
 app.post("/chat", async (req, res) => {
