9a8e9213e9df18f4fea86dc70b9cfa851308d5c4
Norman Qian <normanqian@gmail.com>
2024-11-12 20:32:14 -0500

    18k

--- index.ts
+++ index.ts
@@ -183,7 +183,7 @@ app.post("/ragchat", async (req, res) => {
           Authorization: `Bearer ${API_KEY}`,
         },
         body: JSON.stringify({
-          model: model || "hermes3-405b-fp8-128k",
+          model: model || "hermes3-405b",
           messages: augmentedMessages,
         }),
       });
@@ -225,7 +225,7 @@ app.post("/chat", async (req, res) => {
         Authorization: `Bearer ${API_KEY}`,
       },
       body: JSON.stringify({
-        model: model || "hermes3-405b-fp8-128k",
+        model: model || "hermes3-405b",
         messages: messages,
       }),
     });
